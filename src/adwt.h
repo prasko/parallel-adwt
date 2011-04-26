155e88b6e24330f45e8b2ea63f7b00540110fe9e
Matija Osrecki <mosrecki@gmail.com>
2011-04-26 16:25:42 +0200

    Just a begging of some refactoring

---
+++ src/adwt.h
@@ -0,0 +1,20 @@
+/*
+  Copyright: (c) 2011 Matija Osrecki <matija.osrecki@fer.hr>
+*/
+
+#ifndef ADWT_ADWT_H
+#define ADWT_ADWT_H
+
+class WaveletTransform {
+public:
+  
+};
+
+class AdaptiveWT {
+public:
+  AdaptiveWT(Lpw &lpw) : lpw_(lpw) {}
+  void doo() {}
+}
+
+
+#endif  // ADWT_ADWT_H
